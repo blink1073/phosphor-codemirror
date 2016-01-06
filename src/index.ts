15f29c08ef02e474d400792fc3ce171f57cde913
Steven Silvester <steven.silvester@ieee.org>
2016-01-06 10:35:19 -0600

    Clean up the api

--- src/index.ts
+++ src/index.ts
@@ -14,9 +14,6 @@ import 'codemirror/mode/meta';
 
 import 'codemirror/lib/codemirror.css';
 
-import * as dmp
-  from 'diff-match-patch';
-
 import {
   Message
 } from 'phosphor-messaging';
@@ -29,12 +26,7 @@ import {
 /**
  * The class name added to CodeMirrorWidget instances.
  */
-const EDITOR_CLASS = 'jp-CodeMirroWidget';
-
-/**
- * Initialize diff match patch.
- */
-let diffMatchPatch = new dmp.diff_match_patch();
+const EDITOR_CLASS = 'p-CodeMirrorWidget';
 
 
 /**
@@ -50,9 +42,6 @@ class CodeMirrorWidget extends Widget {
     super();
     this.addClass(EDITOR_CLASS);
     this._editor = CodeMirror(this.node, options);
-    if (options && options.mode) {
-       this.setModeByName(options.mode);
-    }
   }
 
   /**
@@ -66,80 +55,21 @@ class CodeMirrorWidget extends Widget {
    }
 
   /**
-   * Update the text in the widget.
-   */
-  updateText(text: string): void {
-    this._dirtyText = text;
-    if (!this.isAttached || !this.isVisible) {
-      return;
-    }
-    this.update();
-  }
-
-  /**
-   * Set the mode by name.
-   */
-  setModeByName(name: string) {
-    if (CodeMirror.mimeModes.hasOwnProperty(name)) {
-      this._editor.setOption('mode', name);
-    } else {
-      let info = CodeMirror.findModeByName(name);
-      if (info) {
-        this.loadCodeMirrorMode(info.mode, name);
-      }
-    }
-  }
-
-  /**
-   * Set the mode by given the mimetype.
-   *
-   * #### Notes
-   * Valid mimetypes are listed in https://github.com/codemirror/CodeMirror/blob/master/mode/meta.js.
-   */
-  setModeByMimetype(mimetype: string): void {
-    if (CodeMirror.mimeModes.hasOwnProperty(mimetype)) {
-      this._editor.setOption('mode', mimetype);
-    } else {
-      let info = CodeMirror.findModeByMIME(mimetype);
-      if (info) {
-        this.loadCodeMirrorMode(info.mode, mimetype);
-      }
-    }
-  }
-
-  /**
-   * Set the mode by the given filename.
-   */
-  setModeByFilename(filename: string): void {
-    let info = CodeMirror.findModeByFileName(filename);
-    if (info) {
-      this.loadCodeMirrorMode(info.mode, info.mime);
-    }
-  }
-
-  /**
    * A message handler invoked on an `'after-attach'` message.
    */
   protected onAfterAttach(msg: Message): void {
     if (!this.isVisible) {
-      this._dirtyText = this._editor.getDoc().getValue();
+      this._dirty = true;
       return;
     }
-    if (this._dirtyText.length) {
-      this.update();
-      this._editor.refresh();
-    }
+    this._editor.refresh();
   }
 
   /**
    * A message handler invoked on an `'after-show'` message.
    */
   protected onAfterShow(msg: Message): void {
-    if (this._dirtyText.length) {
-      this.update();
-      this._editor.refresh();
-    }
-
+    if (this._dirty) this._editor.refresh();
   }
 
   /**
@@ -153,86 +83,6 @@ class CodeMirrorWidget extends Widget {
     }
   }
 
-  /**
-   * A message handler invoked on an `'update-request'` message.
-   *
-   * #### Notes
-   * Attempts to restore the cursor to the correct
-   * place by using the bitap algorithm to find the corresponding
-   * position of the cursor in the new text.
-   */
-  protected onUpdateRequest(msg: Message): void {
-    let doc = this._editor.getDoc();
-    let oldText = doc.getValue();
-    let text = this._dirtyText;
-    this._dirtyText = '';
-    if (oldText !== text) {
-      // TODO: do something smart with all the selections
-
-      let oldCursor = doc.indexFromPos(doc.getCursor());
-      let cursor = 0;
-      if (oldCursor === oldText.length) {
-        // if the cursor was at the end, keep it at the end
-        cursor = text.length;
-      } else {
-        let fragment = oldText.substr(oldCursor, 10);
-        cursor = diffMatchPatch.match_main(text, fragment, oldCursor);
-      }
-
-      doc.setValue(text);
-      doc.setCursor(doc.posFromIndex(cursor));
-    }
-  }
-
-  /**
-   * Load and set a CodeMirror mode.
-   *
-   * #### Notes
-   * This assumes WebPack as the module loader.
-   * It can be overriden by subclasses.
-   */
-  protected loadCodeMirrorMode(mode: string, mimetype: string): void {
-    let editor = this._editor;
-    if (CodeMirror.modes.hasOwnProperty(mode)) {
-      editor.setOption('mode', mimetype);
-    } else {
-      // Bundle common modes.
-      switch(mode) {
-      case 'python':
-        require('codemirror/mode/python/python');
-        editor.setOption('mode', mimetype);
-        break;
-      case 'javascript':
-      case 'typescript':
-        require('codemirror/mode/javascript/javascript');
-        editor.setOption('mode', mimetype);
-        break;
-      case 'css':
-        require('codemirror/mode/css/css');
-        editor.setOption('mode', mimetype);
-        break;
-      case 'julia':
-        require('codemirror/mode/julia/julia');
-        editor.setOption('mode', mimetype);
-        break;
-      case 'r':
-        require('codemirror/mode/r/r');
-        editor.setOption('mode', mimetype);
-        break;
-      case 'markdown':
-        require('codemirror/mode/markdown/markdown');
-        editor.setOption('mode', mimetype);
-        break;
-      default:
-        // Load the remaining mode bundle asynchronously.
-        require([`codemirror/mode/${mode}/${mode}.js`], () => {
-          editor.setOption('mode', mimetype);
-        });
-        break;
-      }
-    }
-  }
-
   private _editor: CodeMirror.Editor = null;
-  private _dirtyText = '';
+  private _dirty = true;
 }
