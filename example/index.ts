316b960cfd0cd6617c129971f6cceef8916aa2ed
Steven Silvester <steven.silvester@ieee.org>
2016-01-06 09:32:53 -0600

    Add front matter

---
+++ example/index.ts
@@ -0,0 +1,24 @@
+/*-----------------------------------------------------------------------------
+| Copyright (c) 2014-2015, PhosphorJS Contributors
+|
+| Distributed under the terms of the BSD 3-Clause License.
+|
+| The full license is in the file LICENSE, distributed with this software.
+|----------------------------------------------------------------------------*/
+'use strict';
+
+import {
+  Widget
+} from 'phosphor-widget';
+
+import './index.css';
+
+
+
+function main(): void {
+
+
+}
+
+
+window.onload = main;
